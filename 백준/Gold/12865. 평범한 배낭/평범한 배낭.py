02c887ee2f1ccdaff703e4e666d04217e387ce30
Beombeom <97931167+Pooky112@users.noreply.github.com>
2024-02-05 22:55:08 +0900

    [Gold V] Title: 평범한 배낭, Time: 2264 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,23 +1,21 @@
-N, K = map(int, input().split())
+import sys
 
-items = []
-for _ in range(N):
-    W, V = map(int, input().split())
-    items.append((W, V))
+N, K = map(int, sys.stdin.readline().split())
+items = [tuple(map(int,sys.stdin.readline().split())) for _ in range(N)]
 
 def max_value(N, K, items):
-    dp = [[0 for _ in range(K+1)] for _ in range(N+1)]
+    """
+    N = number of items
+    K = Maximum weight capacity
+    items = (weight, value) of item
+    """
+    dp = [0]*(K + 1)
 
-    for i in range(1, N+1):
-        for w in range(1, K+1):
-            # 넣을 수 있는 경우
-            if items[i-1][0] <= w:
-                dp[i][w] = max(dp[i-1][w], dp[i-1][w-items[i-1][0]]+ items[i-1][1])
-            # 넣을 수 없는 경우
-            else:
-                dp[i][w] = dp[i-1][w]
+    for item_index in range(N):
+        for current_weight in range(K, items[item_index][0]- 1, -1):
+            dp[current_weight] = max(dp[current_weight], dp[current_weight - items[item_index][0]] + items[item_index][1])
 
-    return dp[N][K]
+    return dp[K]
 
 best_value = max_value(N, K, items)
 print(best_value)
