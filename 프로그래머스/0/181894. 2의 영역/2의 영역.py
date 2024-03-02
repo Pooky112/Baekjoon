0f2e4d63121625800769250daac33066967da0ee
Beombeom <97931167+Pooky112@users.noreply.github.com>
2024-03-02 20:16:36 +0900

    [level 0] Title: 2의 영역, Time: 0.55 ms, Memory: 15.7 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -2,8 +2,8 @@ def solution(arr):
     if 2 not in arr:
         return [-1]
     
-    num = [i for i, x in enumerate(arr) if x == 2]
+   # num = [i for i, x in enumerate(arr) if x == 2]
     
-    return arr[min(num):max(num)+1]
-    
+    #return arr[min(num):max(num)+1]
+    return arr[arr.index(2) : len(arr) - arr[::-1].index(2)]
     
