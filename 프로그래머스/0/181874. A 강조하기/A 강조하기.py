ac94e18b700dde4837b8654fb6800b5efba1ce09
Beombeom <97931167+Pooky112@users.noreply.github.com>
2024-03-03 15:44:55 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -0,0 +1,13 @@
+def solution(myString):
+    ans = ''
+    for c in myString:
+        if c == 'a':
+            ans += 'A'
+        elif c != 'A' and c.isupper():
+            ans += c.lower()
+        else:
+            ans += c
+            
+    return ans
+    
+    
