f54cd8550d5380e666cbaebc9ee830b29a246eed
Beombeom <97931167+Pooky112@users.noreply.github.com>
2024-03-04 17:15:05 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -7,3 +7,5 @@ def solution(myString, pat):
             changed += "A"
     
     return 1 if pat in changed else 0
+
+"return int(pat in myString.replace('A', 'C').replace('B', 'A').replace('C', 'B'))"
