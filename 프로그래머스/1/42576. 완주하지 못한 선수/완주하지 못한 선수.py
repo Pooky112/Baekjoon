c782cf0ced6543ba5e3328b7a3d74efaac7e6836
Beombeom <97931167+Pooky112@users.noreply.github.com>
2024-03-05 20:10:55 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 41.53 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,7 +1,35 @@
-from collections import Counter
+#from collections import Counter
 
 def solution(participant, completion):
-    par = Counter(participant)
-    com = Counter(completion)
+    #par = Counter(participant)
+    #com = Counter(completion)
     
-    return list((par - com).keys())[0]
+    #return list((par - com).keys())[0]
+    h_table = {}
+    for name in participant:
+        if name in h_table:
+            h_table[name] += 1
+        else:
+            h_table[name] = 1
+    
+    for name in completion:
+        h_table[name] -= 1
+        
+    for name, count in h_table.items():
+        if count > 0:
+            return name
+
+
+
+"""
+    answer = ''
+    temp = 0
+    dic = {}
+    for part in participant:
+        dic[hash(part)] = part
+        temp += int(hash(part))
+    for com in completion:
+        temp -= hash(com)
+    answer = dic[temp]
+
+    return answer"""
