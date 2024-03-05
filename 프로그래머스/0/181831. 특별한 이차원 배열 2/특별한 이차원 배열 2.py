345a41ad51d5acc8337f7d6303ac5708fc8b7abc
Beombeom <97931167+Pooky112@users.noreply.github.com>
2024-03-05 10:14:53 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
@@ -1,8 +1,13 @@
 def solution(arr):
-
+    z = list(map(list, zip(*arr)))
+    print(z)
     for i in range(len(arr)):
         for j in range(len(arr)):
             if arr[i][j] != arr[j][i]:
                 return 0
     
     return 1
+
+"""
+return int(arr == list(map(list, zip(*arr))))
+"""
