60e71c9f63a6561d184347422513599fb9441c74
Beombeom <97931167+Pooky112@users.noreply.github.com>
2024-03-06 14:19:36 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,21 +1,14 @@
-import itertools
-
 def solution(clothes):
-    #answer = 0
     cloth_table = {}
     for name, cloth in clothes:
         if cloth in cloth_table:
             cloth_table[cloth] += 1
         else:
             cloth_table[cloth] = 1
-    #val = list(cloth_table.values())
     
-    #for i in range(1, len(cloth_table)+1):
-     #   temp = list(itertools.combinations(val, i))
-      #  for j in range(len(temp)):
-       #     answer += temp[j][0]
     answer = 1
     for count in cloth_table.values():
+        print(count)
         answer *= (count + 1)
         
     return answer - 1
