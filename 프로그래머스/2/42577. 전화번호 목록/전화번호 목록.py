7d3d55eaeb827546726307290d99a7e12c39a7cc
Beombeom <97931167+Pooky112@users.noreply.github.com>
2024-03-06 12:41:16 +0900

    [level 2] Title: 전화번호 목록, Time: 168.84 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,7 +1,20 @@
 def solution(phone_book):
+    """
     phone_book.sort()
     for i in range(len(phone_book) -1):
         if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
             return False
             
     return True
+
+"""
+    p_table = set(phone_book)
+    for num in phone_book:
+        temp = ''
+        for pnum in num:
+            temp += pnum
+            if temp in p_table and temp != num:
+                return False
+    return True
+        
+        
