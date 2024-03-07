7d732d53965f321878af66258d924eca9fca3a82
Beombeom <97931167+Pooky112@users.noreply.github.com>
2024-03-07 20:45:56 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -0,0 +1,23 @@
+def solution(bridge_length, weight, truck_weights):
+    ans = 0
+    bridge_queue = []
+    bridge_weight = 0
+    
+    while truck_weights or bridge_queue:
+        ans += 1
+        
+        if bridge_queue and bridge_queue[0][1] == ans: 
+            bridge_weight -= bridge_queue[0][0]
+            bridge_queue.pop(0)
+        
+        if truck_weights and bridge_weight + truck_weights[0] <= weight:
+            truck = truck_weights.pop(0)
+            bridge_weight += truck
+            bridge_queue.append((truck, bridge_length + ans))
+        
+    return ans
+            
+        
+            
+        
+    return ans
