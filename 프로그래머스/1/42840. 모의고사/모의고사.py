7fe70be088d393375db376e9dbf289d32a25711c
Beombeom <97931167+Pooky112@users.noreply.github.com>
2024-03-09 12:39:06 +0900

    [level 1] Title: 모의고사, Time: 2.57 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,29 +1,21 @@
 def solution(answers):
-    answer = []
-    num_1 = [1, 2, 3, 4, 5] * (len(answers)// 5 + 1)
-    num_2 = [2, 1, 2, 3, 2, 4, 2, 5] * (len(answers)// 8 + 1)
-    num_3 = [3, 3, 1, 1,2,  2,4, 4,5, 5] * (len(answers)// 5 + 1)
-    a, b, c = 0, 0, 0
-    for val, ans in zip(num_1[:len(answers)], answers):
-        print("a", val, ans)
-        if val == ans:
-            a += 1
-    for val, ans in zip(num_2[:len(answers)], answers):
-        print("b", val, ans)
-        if val == ans:
-            b += 1
-    for val, ans in zip(num_3[:len(answers)], answers):
-        print("c", val, ans)
-        if val == ans:
-            c += 1
+    num_1 = [1, 2, 3, 4, 5]
+    num_2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    num_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
-    if max(a,b,c) == a:
-        answer.append(1)
-    if max(a,b,c) == b:
-        answer.append(2)
-    if max(a,b,c) == c:
-        answer.append(3)
+    scores = [0, 0, 0]
     
+    for i, ans in enumerate(answers):
+        if ans == num_1[i % 5]:
+            scores[0] += 1
+    for i, ans in enumerate(answers):
+        if ans == num_2[i % 8]:
+            scores[1] += 1
+    for i, ans in enumerate(answers):
+        if ans == num_3[i % 10]:
+            scores[2] += 1
+    
+    answer = [i + 1 for i, score in enumerate(scores) if score == max(scores)]
     
     return answer
     
