c087d44f04ff88ed689587f819cf5051e70d8a32
Beombeom <97931167+Pooky112@users.noreply.github.com>
2024-03-21 17:06:18 +0900

    [level 2] Title: 타겟 넘버, Time: 5.56 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,13 +1,19 @@
 def solution(numbers, target):
-    #재귀함수 정의
     def count_targets(index, curr_sum):
         if index == len(numbers):
             return 1 if curr_sum == target else 0
-    
-        count = 0
-        count += count_targets(index + 1, curr_sum + numbers[index])
-        count += count_targets(index + 1, curr_sum - numbers[index])
-        return count
+        
+        return count_targets(index + 1, curr_sum + numbers[index]) + count_targets(index + 1, curr_sum - numbers[index])
     
     
     return count_targets(0, 0)
+
+"""
+def solution(numbers, target):
+    if not numbers and target == 0 :
+        return 1
+    elif not numbers:
+        return 0
+    else:
+        return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
+"""
