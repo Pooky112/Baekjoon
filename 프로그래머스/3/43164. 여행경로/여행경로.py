cea0d56f51969596a6fd530320fb55f25c1dc047
Beombeom <97931167+Pooky112@users.noreply.github.com>
2024-03-25 14:54:57 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -8,13 +8,18 @@ def solution(tickets):
             routes[start].append(end)
         else:
             routes[start] = [end]
-            
-    stack = ["ICN"]
-    while stack:
-        top = stack[-1]
-        if top not in routes or len(routes[top]) == 0:
-            answer.append(stack.pop())
-        else:
-            stack.append(routes[top].pop())
-            
+    
+    def dfs(airport):
+        while airport in routes and routes[airport]:
+            next_airport = routes[airport].pop()
+            dfs(next_airport)
+        answer.append(airport)
+    # stack = ["ICN"]
+    # while stack:
+    #     top = stack[-1]
+    #     if top not in routes or len(routes[top]) == 0:
+    #         answer.append(stack.pop())
+    #     else:
+    #         stack.append(routes[top].pop())
+    dfs("ICN")
     return answer[::-1]
