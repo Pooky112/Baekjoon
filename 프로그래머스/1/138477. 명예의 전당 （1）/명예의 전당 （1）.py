81afa4ba2778a935f3a72754d76d47ecefec4a2a
Beombeom <97931167+Pooky112@users.noreply.github.com>
2024-04-03 16:06:24 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -0,0 +1,14 @@
+def solution(k, score):
+    hall_of_fame = [] 
+    answer = []
+    
+    for s in score:
+        hall_of_fame.append(s)
+        hall_of_fame.sort(reverse=True)
+        
+        if len(hall_of_fame) > k:
+            hall_of_fame = hall_of_fame[:k]
+        
+        answer.append(hall_of_fame[-1])
+    
+    return answer
