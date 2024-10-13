19ae9cedaa9201788afd3d189dc3845593c0667e
JeongBeom Lim <97931167+Pooky112@users.noreply.github.com>
2024-10-14 01:07:11 +0900

    [level 0] Title: l로 만들기, Time: 23.82 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,13 +1,14 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        for(char c : myString.toCharArray()){
-            if (c < 'l'){
-                answer += 'l';
-            } else{
-                answer += c;
-            }
-        }
-        return answer;
+        return myString.replaceAll("[^l-z]", "l");
+        // String answer = "";
+        // for(char c : myString.toCharArray()){
+        //     if (c < 'l'){
+        //         answer += 'l';
+        //     } else{
+        //         answer += c;
+        //     }
+        // }
+        // return answer;
     }
 }
