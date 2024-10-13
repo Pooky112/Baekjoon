e439046811cfe5ad2980f038facf4f0a909e6c1e
JeongBeom Lim <97931167+Pooky112@users.noreply.github.com>
2024-10-13 20:40:41 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1316 ms, Memory: 131644 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,55 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
-//    public static void quickSort(int[] arr, int low, int high){
-//        if (low >= high){
-//            return;
-//        }
-//
-//        int pivot = low + (high - low) / 2;
-//        int pivotValue = arr[pivot];
-//
-//        int left = low;
-//        int right = high;
-//        while (left <= right){
-//            while (arr[left] < pivotValue){
-//                left++;
-//            }
-//            while (arr[right] > pivotValue){
-//                right--;
-//            }
-//
-//            if (left <= right){
-//                int temp = arr[right];
-//                arr[right] = arr[left];
-//                arr[left] = temp;
-//                left++;
-//                right--;
-//            }
-//        }
-//
-//        quickSort(arr, low, right);
-//        quickSort(arr, left, high);
-//
-//    }
     public static void main(String[] args) throws IOException {
-//        Scanner sc = new Scanner(System.in);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        int[] numList = new int[N];
-        for (int i = 0; i < N; i++){
-            numList[i] = Integer.parseInt(br.readLine());
-        }
-        Arrays.sort(numList);
-//        quickSort(numList, 0, N - 1);
 
-        for (int num: numList){
-            System.out.println(num);
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(br.readLine()));
         }
+
+        Collections.sort(list);
+
+        for(int num : list){
+            sb.append(num).append('\n');
+        }
+        System.out.println(sb);
     }
 }
